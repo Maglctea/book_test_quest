18395fa62a07a901895ac71d00cedd86b036dcc4
Magictea <sherbakovdaniil6@gmail.com>
2024-11-21 19:33:35 +0300

    feat: added auto create folder for cvs file

--- src/book/management/commands/start_export_books_worker.py
+++ src/book/management/commands/start_export_books_worker.py
@@ -1,4 +1,6 @@
 # your_app/management/commands/your_command.py
+import os
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from book.workers import ExportBookTaskWorker
@@ -12,8 +14,14 @@ class Command(BaseCommand):
         parser.add_argument('books_limit', type=int, default=30, help='Count books to process in batch')
 
     def handle(self, *args, **kwargs):
+
         tasks_limit: int = kwargs['tasks_limit']
         books_limit: int = kwargs['books_limit']
         worker = ExportBookTaskWorker(tasks_limit, books_limit)
+
+        csv_path = os.path.join(settings.MEDIA_ROOT, 'csv')
+        if not os.path.exists(csv_path):
+            os.makedirs(csv_path)
+
         print('START WORKER...')
         worker.run()
